832196d017f8be2d57cf787a155f12e5a65f5fed
MuqeemNasir <mac786m@gmail.com>
2025-09-04 01:31:04 +0530

    add get routes in express to fetch data

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const express = require('express')
+const app = express()
+
 const { default: mongoose, get, mongo } = require('mongoose');
 const { initializeDatabase } = require('./db/db.connect')
 const Restaurant = require('./models/restaurant.models')
+
+app.use(express.json())
 
 initializeDatabase()
 
@@ -20,71 +25,83 @@ initializeDatabase()
 //   photos: ["https://example.com/photo1.jpg", "https://example.com/photo2.jpg"],
 // };
 
-const somiRestaurant = {
-    name: "Somi",
-    cuisine: ["Greek"],
-    location: "11 Main Road, Gem",
-    rating: 4.3,
-    reviews: [],
-    website: "https://somi-example.com",
-    phoneNumber: "+1234997390",
-    openHours: "Tue-Sun: 11:00 AM - 10:00 PM",
-    priceRange: "$$ (11-30)",
-    reservationsNeeded: false,
-    isDeliveryAvailable: true,
-    menuUrl: "https://somi-example.com/menu",
-    photos: ["https://example.com/somi-photo1.jpg", "https://example.com/somi-photo2.jpg"],
-};
+// const somiRestaurant = {
+//     name: "Somi",
+//     cuisine: ["Greek"],
+//     location: "11 Main Road, Gem",
+//     rating: 4.3,
+//     reviews: [],
+//     website: "https://somi-example.com",
+//     phoneNumber: "+1234997390",
+//     openHours: "Tue-Sun: 11:00 AM - 10:00 PM",
+//     priceRange: "$$ (11-30)",
+//     reservationsNeeded: false,
+//     isDeliveryAvailable: true,
+//     menuUrl: "https://somi-example.com/menu",
+//     photos: ["https://example.com/somi-photo1.jpg", "https://example.com/somi-photo2.jpg"],
+// };
 
-const yoChinaRestaurant = {
-    name: "Yo China",
-    cuisine: ["Chinese", "Italian"],
-    location: "MG Road, Bangalore",
-    rating: 3.9,
-    reviews: [],
-    website: "https://yo-example.com",
-    phoneNumber: "+1288997392",
-    openHours: "Tue-Sun: 10:00 AM - 11:00 PM",
-    priceRange: "$$$ (31-60)",
-    reservationsNeeded: true,
-    isDeliveryAvailable: false,
-    menuUrl: "https://yo-example.com/menu",
-    photos: ["https://example.com/yo-photo1.jpg", "https://example.com/yo-photo2.jpg", "https://example.com/yo-photo3.jpg"]
-};
+// const yoChinaRestaurant = {
+//     name: "Yo China",
+//     cuisine: ["Chinese", "Italian"],
+//     location: "MG Road, Bangalore",
+//     rating: 3.9,
+//     reviews: [],
+//     website: "https://yo-example.com",
+//     phoneNumber: "+1288997392",
+//     openHours: "Tue-Sun: 10:00 AM - 11:00 PM",
+//     priceRange: "$$$ (31-60)",
+//     reservationsNeeded: true,
+//     isDeliveryAvailable: false,
+//     menuUrl: "https://yo-example.com/menu",
+//     photos: ["https://example.com/yo-photo1.jpg", "https://example.com/yo-photo2.jpg", "https://example.com/yo-photo3.jpg"]
+// };
 
-async function createRestaurant(newRestaurant) {
-    try {
-        const restaurant = new Restaurant(newRestaurant)
-        const saveRestaurant = await restaurant.save()
-        console.log("New Restaurant data: ", saveRestaurant)
-    } catch (error) {
-        throw error
-    }
-}
+// async function createRestaurant(newRestaurant) {
+//     try {
+//         const restaurant = new Restaurant(newRestaurant)
+//         const saveRestaurant = await restaurant.save()
+//         console.log("New Restaurant data: ", saveRestaurant)
+//     } catch (error) {
+//         throw error
+//     }
+// }
 
-// Question 1 & 2
+// // Question 1 & 2
 
-async function seedRestaurant() {
-    await createRestaurant(somiRestaurant);
-    await createRestaurant(yoChinaRestaurant);
-    mongoose.connection.close()
-}
+// async function seedRestaurant() {
+//     await createRestaurant(somiRestaurant);
+//     await createRestaurant(yoChinaRestaurant);
+//     mongoose.connection.close()
+// }
 
-// seedRestaurant()
+// // seedRestaurant()
 
 // Question 3:
 
 async function readAllRestaurant() {
     try {
         const allRes = await Restaurant.find()
-        console.log("All Restaurants Details: ", allRes)
-        mongoose.connection.close()
-
+        // console.log("All Restaurants Details: ", allRes)
+        return allRes
     } catch (error) {
         console.log(error)
-        mongoose.connection.close()
     }
 }
+
+app.get("/restaurants", async(req, res) => {
+    try{
+        const restaurants = await readAllRestaurant()
+        if(restaurants.length !== 0){
+            res.json(restaurants)
+        }else{
+            res.status(404).json({error: "No Restaurants found."})
+        }
+    }catch(error){
+        res.status(500).json({error: "Failed to fetch restaurants."})
+    }
+})
+
 
 // readAllRestaurant()
 
@@ -94,16 +111,30 @@ async function getRestaurantByName(restaurantName) {
     try {
         const restaurant = await Restaurant.findOne({ name: restaurantName })
         if (restaurant) {
-            console.log("Restaurant Details: ", restaurant)
-            mongoose.connection.close()
+            // console.log("Restaurant Details: ", restaurant)
+            // mongoose.connection.close()
+            return restaurant
         } else {
             console.log(`No restaurant found with the name "${restaurantName}"`)
-            mongoose.connection.close()
+            // mongoose.connection.close()
         }
     } catch (error) {
         console.log(error)
     }
 }
+
+app.get("/restaurants/:restaurantName", async(req, res) => {
+    try{
+        const restaurant = await getRestaurantByName(req.params.restaurantName)
+        if(restaurant.length !== 0){
+            res.json(restaurant)
+        }else{
+            res.status(404).json({error: "Restaurant not found by name."})
+        }
+    }catch(error){
+        res.status(500).json({error: "Failed to fetch restaurants by name."})
+    }
+})
 
 // getRestaurantByName("New Restaurant")
 
@@ -152,18 +183,32 @@ async function getRestaurantByPhoneNumber(phoneNumber) {
     try {
         const restaurantPhoneNumber = await Restaurant.findOne({ phoneNumber: phoneNumber })
         if (restaurantPhoneNumber) {
-            console.log(`Restaurant details of phone number "${phoneNumber}": `, restaurantPhoneNumber)
-            mongoose.connection.close()
+            // console.log(`Restaurant details of phone number "${phoneNumber}": `, restaurantPhoneNumber)
+            // mongoose.connection.close()
+            return restaurantPhoneNumber
         }
         else {
             console.log(`No Restaurant found with the "${phoneNumber}".`)
-            mongoose.connection.close()
+            // mongoose.connection.close()
         }
     } catch (error) {
         console.log(error)
-        mongoose.connection.close()
+        // mongoose.connection.close()
     }
 }
+
+app.get("/restaurants/directory/:phoneNumber", async(req, res) => {
+    try{
+        const restaurant = await getRestaurantByPhoneNumber(req.params.phoneNumber)
+        if(restaurant.length !== 0){
+            res.json(restaurant)
+        }else{
+            res.status(404).json({error: "Restaurant not found by phone number."})
+        }
+    }catch(error){
+        res.status(500).json({error: "Failed to fetch restaurants by phone number."})
+    }
+})
 
 // getRestaurantByPhoneNumber("+1288997392")
 
@@ -173,20 +218,57 @@ async function getRestaurantByCuisine(cuisineName){
     try{
         const cuisineRestaurant = await Restaurant.find({cuisine: cuisineName})
         if(cuisineRestaurant){
-            console.log(`Restaurants details with "${cuisineName}" cuisine: `, cuisineRestaurant)
-            mongoose.connection.close()
+            // console.log(`Restaurants details with "${cuisineName}" cuisine: `, cuisineRestaurant)
+            // mongoose.connection.close()
+            return cuisineRestaurant
         }else{
             console.log(`No Restaurant found with "${cuisineName}" cuisine.`)
-            mongoose.connection.close()
+            // mongoose.connection.close()
         }
     }catch(error){
         console.log(error)
-        mongoose.connection.close()
+        // mongoose.connection.close()
     }
 }
 
+app.get("/restaurants/cuisine/:cuisineName", async(req, res) => {
+    try{
+        const restaurants = await getRestaurantByCuisine(req.params.cuisineName)
+        if(restaurants.length !== 0){
+            res.json(restaurants)
+        }else{
+            res.status(404).json({error: "Restaurant not found by cuisine."})
+        }
+    }catch(error){
+        res.status(500).json({error: "Failed to fetch restaurants by cuisine."})
+    }
+})
 
 // getRestaurantByCuisine("Italian")
+
+async function getRestaurantByLocation(resLocation){
+    try{
+        const restaurant = await Restaurant.find({location: resLocation})
+        if(restaurant){
+            return restaurant
+        }
+    }catch(error){
+        console.log("Error in getting location restaurant.")
+    }
+}
+
+app.get("/restaurants/location/:restaurantLocation", async(req, res) => {
+    try{
+        const restaurant = await getRestaurantByLocation(req.params.restaurantLocation)
+        if(restaurant.length !== 0){
+            res.json(restaurant)
+        }else{
+            res.status(404).json({error: "Restaurant not found by location."})
+        }
+    }catch(error){
+        res.status(500).json({error: "Failed to fetch restaurant by location"})
+    }
+})
 
 // BE2.3_HW1 
 
@@ -253,4 +335,10 @@ async function deleteRestaurantByName(resName){
     }
 }
 
-deleteRestaurantByName("Yo China")
+// deleteRestaurantByName("Yo China")
+
+const PORT = 3000
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+})
